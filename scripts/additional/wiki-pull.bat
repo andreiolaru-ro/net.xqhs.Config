83c8f6b68ffde5db5de968dd3374b86da37e572f
andreiolaru <cs@andreiolaru.ro>
2013-02-04 19:42:51 +0200

    Added and improved scripts.

--- scripts/additional/wiki-pull.bat
+++ scripts/additional/wiki-pull.bat
@@ -1,18 +1,17 @@
-@echo Pushing changes to the wiki.
+@echo Pulling changes from the wiki.
 @echo This assumes you have git installed on your system and in PATH (executing the command 'git' in console writes the possible git operations).
 @echo If anything goes wrong, please use the command line to execute commands in this file.
 @echo Working in:
 @cd
 @echo ====================================
-@echo Ready to commit...
+@echo Ready to fetch (ready to insert passphrase)...
 @pause
-@git add -u .
-@git commit -m "updated wiki"
+@ssh-agent bash -c "ssh-add $HOME/.ssh/githubkey; git fetch"
 @git status
 @echo ====================================
-@echo If anything to commit, ready to push (ready to insert passphrase); otherwise close this window
+@echo Ready to pull (ready to insert passphrase); otherwise close this window
 @pause
-@ssh-agent bash -c "ssh-add $HOME/.ssh/githubkey; git push"
+@ssh-agent bash -c "ssh-add $HOME/.ssh/githubkey; git pull"
 @echo ====================================
 @echo Supposedly done.
 @pause
